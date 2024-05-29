ded6121fbe18e2141466515d92dd4cdbb909c177
kirandash <thekirankumardash@gmail.com>
2024-05-30 02:12:30 +0800

    6 Setup MongoDB

--- chatterbox-be/src/app.module.ts
+++ chatterbox-be/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from './common/database/database.module';
 @Module({
-  imports: [],
+  imports: [
+    // This will load the .env file and make it available to the app
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
