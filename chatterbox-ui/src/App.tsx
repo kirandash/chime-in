77f6a99a96db32ff5975afa09fb68496e1dd449e
kirandash <thekirankumardash@gmail.com>
2024-05-30 00:11:52 +0800

    1.2 Implement Dark Theme using MUI and some base styling

--- chatterbox-ui/src/App.tsx
+++ chatterbox-ui/src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
-  return <div>Chatter Box!</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      {/* CSS Baseline for some default styling */}
+      <CssBaseline />
+      <Container>
+        <h1>Chatter Box!</h1>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
