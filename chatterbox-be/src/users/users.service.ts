1bc756facdfadbd3a0c4ad3e7fd53f38df74f844
kirandash <thekirankumardash@gmail.com>
2024-06-01 02:53:28 +0800

    fix: update user password hash check for undefined

--- chatterbox-be/src/users/users.service.ts
+++ chatterbox-be/src/users/users.service.ts
@@ -30,12 +30,16 @@ export class UsersService {
   }
 
   async update(_id: string, updateUserInput: UpdateUserInput) {
+    if (updateUserInput.password) {
+      updateUserInput.password = await this.hashPassword(
+        updateUserInput.password,
+      );
+    }
     return this.usersRepository.findOneAndUpdate(
       { _id },
       {
         $set: {
           ...updateUserInput,
-          password: await this.hashPassword(updateUserInput.password),
         },
       },
     );
