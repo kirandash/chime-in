c9e97dcbbf77e1332d1648f36ca147b83cda1958
kirandash <thekirankumardash@gmail.com>
2024-06-23 21:49:18 +0800

    fix: import ChatSchema in chats module

--- chime-in-be/src/chats/chats.module.ts
+++ chime-in-be/src/chats/chats.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsResolver } from './chats.resolver';
+import { ChatsRepository } from './chats.repository';
+import { DatabaseModule } from '../common/database/database.module';
+import { ChatSchema } from './entities/chat.entity';
 
 @Module({
-  providers: [ChatsResolver, ChatsService],
+  imports: [DatabaseModule.forFeature([{ name: 'Chat', schema: ChatSchema }])],
+  providers: [ChatsResolver, ChatsService, ChatsRepository],
 })
 export class ChatsModule {}
